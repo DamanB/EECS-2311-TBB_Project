179e3135d45f06c108176d10df4dacc5163d8d38
DamanB <damanb19@my.yorku.ca>
2018-01-19 10:43:19 -0500

    Test for conflict

--- Enamel/src/enamel/ToyAuthoring.java
+++ Enamel/src/enamel/ToyAuthoring.java
@@ -4,7 +4,7 @@ import java.io.File;
 import javax.swing.JFileChooser;
 
 public class ToyAuthoring {
-
+	
 	public static void main(String[] args) {
 	    File directory = new File("C:\\Users\\User\\AppData\\Roaming\\SPB_16.6\\git\\EECS-2311-TBB_Project");
 	    JFileChooser explorer = new JFileChooser(directory); 
@@ -13,6 +13,11 @@ public class ToyAuthoring {
     	    ScenarioParser s = new ScenarioParser(true);
 	    	String path = explorer.getSelectedFile().getPath();
     	    s.setScenarioFile(path);
+    	    
+    	    if (returnedValue == 1) {
+    	    	System.out.println("Test");
+    	    }
+    	    
 	    }
 	     // s.setScenarioFile("FactoryScenarios/Scenario_" + 1 + ".txt");
 	}
