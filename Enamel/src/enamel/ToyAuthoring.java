ce24fd86889a316cbaaff31021aa8187aba7e963
Sanjay Paraboo <paraboosanjay@gmail.com>
2018-01-19 10:11:54 -0500

    Testing for conflict

--- Enamel/src/enamel/ToyAuthoring.java
+++ Enamel/src/enamel/ToyAuthoring.java
@@ -2,8 +2,9 @@ package enamel;
 
 public class ToyAuthoring {
 
-    public static void main(String[] args) { 	  	
-    	    ScenarioParser s = new ScenarioParser(true);
-    	    s.setScenarioFile("FactoryScenarios/Scenario_" + 1 + ".txt");
-    }
+	public static void main(String[] args) {
+		ScenarioParser s = new ScenarioParser(true);
+		s.setScenarioFile("FactoryScenarios/Scenario_" + 1 + ".txt");
+		int[] array = new int[5];
+	}
 }
