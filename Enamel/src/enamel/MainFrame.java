7efbca3013b1b6c10304ad17991810e2d09c05b2
guopy96 <guopy96@my.yorku.ca>
2018-01-29 13:58:34 -0500

    I add a main frame, however, it cannot work when I cut the FileChooser part to open(), please fix it

--- Enamel/src/enamel/MainFrame.java
+++ Enamel/src/enamel/MainFrame.java
@@ -1,21 +1,23 @@
 package enamel;
 
 import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import java.awt.event.ActionListener;
-import java.util.LinkedList;
-
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JRadioButton;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class MainFrame {
 	private JFrame frame;
-	LinkedList<JButton> buttonList = new LinkedList<JButton>();
-	JPanel panel;
-	JButton open;
-	JButton create;
+
+	private JPanel panel;
+	private JButton open;
+	private JButton create;
 
 	public MainFrame() {
 		frame = new JFrame();
@@ -28,14 +30,50 @@ public class MainFrame {
 		panel.setSize(200, 50);
 
 		open = new JButton("Open");
-		open.addActionListener(ActionListener());
+		open.addMouseListener(new Click());
 		panel.add(this.open);
 
 		create = new JButton("Create");
+		open.addActionListener(null);
 		this.panel.add(this.create);
 
 		frame.add(panel);
 		frame.repaint();
 		frame.setVisible(true);
+	}
+
+	class Click implements MouseListener {
+		private void open() {
+			// here should be file chooser
+		}
+
+		private void create() {
+			// here should open a new UI
+		}
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			if (e.getButton() == MouseEvent.BUTTON1)
+				if (e.getSource().equals(open))
+					open();
+			if (e.getSource().equals(create))
+				create();
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+		}
 	}
 }
--- Enamel/src/enamel/ToyAuthoring.java
+++ Enamel/src/enamel/ToyAuthoring.java
@@ -1,65 +1,61 @@
 package enamel;
 
-import javax.swing.*;
-import javax.swing.filechooser.FileFilter;
-import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 public class ToyAuthoring {
 
-    //this this the origin version
+	// this this the origin version
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		// MainFrame main = new MainFrame();
+		// Initialising objects for file explorer and the ScenarioParser
+		JButton openfile = new JButton();
+		JFileChooser fileChooser = new JFileChooser();
+		ScenarioParser s;
 
-        // Initialising objects for file explorer and the ScenarioParser
-        JButton open = new JButton();
-        JFileChooser fileChooser = new JFileChooser();
-        ScenarioParser s;
+		// Starts JFileChooser at project directory
+		fileChooser.setCurrentDirectory(new java.io.File("."));
+		// Shows only text files in the file chooser
+		fileChooser.addChoosableFileFilter(new FileNameExtensionFilter("*.txt", "txt"));
 
-        // Starts JFileChooser at project directory
-        fileChooser.setCurrentDirectory(new java.io.File("."));
-
-        // Shows only text files in the file chooser
-        fileChooser.addChoosableFileFilter(new FileNameExtensionFilter("*.txt", "txt"));
-
-        if (fileChooser.showOpenDialog(open) == JFileChooser.APPROVE_OPTION) {
-            s = new ScenarioParser(true);
-
-            // Using regex checks if the file name has the following: ["Scenario_" + (a positive integer) + ".txt"]
-            if (fileChooser.getSelectedFile().getName().matches("^Scenario_[1-9][0-9]*.txt$")) {
-
-                try {
-                    // Reads the file and records the first 2 lines
-                    BufferedReader br = new BufferedReader(new FileReader(fileChooser.getSelectedFile().getAbsolutePath()));
-                    String[] lines = new String[2];
-                    lines[0] = br.readLine();
-                    lines[1] = br.readLine();
-
-                    // Checks if first two lines if they follow the format. Cell + (positive integer) and Button + (positive integer)
-                    if (lines[0].matches("^Cell [1-9][0-9]*$") &&
-                            lines[1].matches("^Button [1-9][0-9]*$")) {
-                        s.setScenarioFile(fileChooser.getSelectedFile().getAbsolutePath());
-                    } else {
-                        // exit
-                        System.out.println("Error: Invalid file format. Cell and Button numbers not found");
-                        return;
-                    }
-
-                } catch (IOException e) {
-                    System.out.println("File error");
-                    return;
-                }
-
-            } else {
-                // exit
-                System.out.println("Error: Invalid file name");
-                return;
-
-            }
-
-        }
-
-    }
+		if (fileChooser.showOpenDialog(openfile) == JFileChooser.APPROVE_OPTION) {
+			s = new ScenarioParser(true);
+			// Using regex checks if the file name has the following: ["Scenario_" + (a
+			// positive integer) + ".txt"]
+			if (fileChooser.getSelectedFile().getName().matches("^Scenario_[1-9][0-9]*.txt$")) {
+				try {
+					// Reads the file and records the first 2 lines
+					BufferedReader br = new BufferedReader(
+							new FileReader(fileChooser.getSelectedFile().getAbsolutePath()));
+					String[] lines = new String[2];
+					lines[0] = br.readLine();
+					lines[1] = br.readLine();
+					// Checks if first two lines if they follow the format. Cell + (positive
+					// integer) and Button + (positive integer)
+					if (lines[0].matches("^Cell [1-9][0-9]*$") && lines[1].matches("^Button [1-9][0-9]*$")) {
+						br.close();
+						// frame.setVisible(false);
+						s.setScenarioFile(fileChooser.getSelectedFile().getAbsolutePath());
+					} else {
+						// exit
+						System.out.println("Error: Invalid file format. Cell and Button numbers not found");
+						return;
+					}
+				} catch (IOException e) {
+					System.out.println("File error");
+					return;
+				}
+			} else {
+				// exit
+				System.out.println("Error: Invalid file name");
+				return;
+			}
+		}
+	}
 }
