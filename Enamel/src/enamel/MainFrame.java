a52d32fcb9757150d6d0a922bcff9b19ca83824c
guopy96 <guopy96@my.yorku.ca>
2018-01-30 15:18:32 -0500

    the bugs

--- Enamel/src/enamel/MainFrame.java
+++ Enamel/src/enamel/MainFrame.java
@@ -8,78 +8,78 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class MainFrame {
-    private JFrame frame;
+	private JFrame frame;
 
-    private JPanel panel;
-    private JButton open;
-    private JButton create;
+	private JPanel panel;
+	private JButton open;
+	private JButton create;
 
-    public MainFrame() {
-        frame = new JFrame();
-        frame.setTitle("Main");
-        frame.setBounds(100, 100, 627, 459);
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.getContentPane().setLayout(new BorderLayout());
+	public MainFrame() {
+		frame = new JFrame();
+		frame.setTitle("Main");
+		frame.setBounds(100, 100, 627, 459);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.getContentPane().setLayout(new BorderLayout());
 
-        panel = new JPanel();
-        panel.setSize(200, 50);
+		panel = new JPanel();
+		panel.setSize(200, 50);
 
-        open = new JButton("Open");
-        open.addMouseListener(new Click());
-        panel.add(this.open);
+		open = new JButton("Open");
+		open.addMouseListener(new Click());
+		panel.add(this.open);
 
-        create = new JButton("Create");
-        create.addMouseListener(new Click());
-        this.panel.add(this.create);
+		create = new JButton("Create");
+		create.addMouseListener(new Click());
+		this.panel.add(this.create);
 
-        frame.add(panel);
-        frame.repaint();
-        frame.setVisible(true);
-    }
+		frame.add(panel);
+		frame.repaint();
+		frame.setVisible(true);
+	}
 
-    public JButton getCreate() {
-        return create;
-    }
+	public JButton getCreate() {
+		return create;
+	}
 
-    public JButton getOpen() {
-        return open;
-    }
+	public JButton getOpen() {
+		return open;
+	}
 
-    class Click implements MouseListener {
-        private void open() {
-            // here should be file chooser
-        }
+	class Click implements MouseListener {
+		private void open() {
+			ToyAuthoring.open();
+		}
 
-        private void create() {
-            ScenarioCreator c = new ScenarioCreator();
-            frame.setVisible(false);
-        }
+		private void create() {
+			ScenarioCreator c = new ScenarioCreator();
+			frame.setVisible(false);
+		}
 
-        @Override
-        public void mouseClicked(MouseEvent e) {
-            if (e.getButton() == MouseEvent.BUTTON1) {
-                if (e.getSource().equals(open)) {
-                    open();
-                } else if (e.getSource().equals(create)) {
-                    create();
-                }
-            }
-        }
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			if (e.getButton() == MouseEvent.BUTTON1) {
+				if (e.getSource().equals(open)) {
+					open();
+				} else if (e.getSource().equals(create)) {
+					create();
+				}
+			}
+		}
 
-        @Override
-        public void mousePressed(MouseEvent e) {
-        }
+		@Override
+		public void mousePressed(MouseEvent e) {
+		}
 
-        @Override
-        public void mouseReleased(MouseEvent e) {
-        }
+		@Override
+		public void mouseReleased(MouseEvent e) {
+		}
 
-        @Override
-        public void mouseEntered(MouseEvent e) {
-        }
+		@Override
+		public void mouseEntered(MouseEvent e) {
+		}
 
-        @Override
-        public void mouseExited(MouseEvent e) {
-        }
-    }
+		@Override
+		public void mouseExited(MouseEvent e) {
+		}
+	}
 }
