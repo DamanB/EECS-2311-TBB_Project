f0a975a1aee01f9d1730f3867e6654cf61f5baf9
Sanjay Paraboo <paraboosanjay@gmail.com>
2018-01-31 18:16:44 -0500

    added node class for creating scenarios

---
+++ Enamel/src/enamel/DragNode.java
@@ -0,0 +1,33 @@
+package enamel;
+
+/**
+ * Created by Sanjay on 1/30/2018.
+ */
+public class DragNode {
+    public enum CommandType {
+        endrepeat,
+        sound,
+        skip,
+        pause,
+        repeat_button,
+        repeat,
+        reset_buttons,
+        skip_buttons,
+        disp_clearAll,
+        disp_cell_pins,
+        disp_string,
+        disp_cell_char,
+        disp_cell_raise,
+        disp_cell_lower,
+        disp_cell_clear,
+        disp_cell_lowerPins,
+        user_input
+
+    }
+
+
+}
+class endRepeat extends DragNode
+{
+
+}
