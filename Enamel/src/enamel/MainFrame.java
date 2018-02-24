77e3a6dae9472e25ea689b669af84c299fac0519
Sanjay Paraboo <paraboosanjay@gmail.com>
2018-02-23 19:49:08 -0500

    Added jar file and dependency libraries

--- Enamel/src/enamel/MainFrame.java
+++ Enamel/src/enamel/MainFrame.java
@@ -17,7 +17,7 @@ public class MainFrame {
 	private static int sizeY = 720;	
 	public static Dimension dimension = new Dimension(sizeX,sizeY);
 	public static Color primColor = new Color(153,197,217);
-	public static boolean isDisposed = false;
+	public static boolean runScen = false;
 
 
 	public MainFrame() {		
@@ -50,7 +50,7 @@ public class MainFrame {
 		mainPanel.add(screen);
 		mainPanel.validate();
 		LoadingScreen.loadingScreen.showLoadingScreen(false);
-		frame.repaint();		
+		frame.repaint();
 	}
 	
 	public static JPanel getMainPanel() {
