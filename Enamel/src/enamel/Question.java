c2aa00a7c0fa45dfe35be7197a0bfd63e4eca680
Sanjay Paraboo <paraboosanjay@gmail.com>
2018-03-13 23:57:22 -0400

    removed useless code

--- Enamel/src/enamel/Question.java
+++ Enamel/src/enamel/Question.java
@@ -33,36 +33,6 @@ public class Question {
         return result;
     }
 
-    /*public void setQuestion(String question) {
-		this.question = question;
-	}
-	
-	public String getQuestion() {return question;}	
-	
-	public int getCellOrientation(int cellIndex) {
-		if (cellIndex >= cells.size()) {
-			throw new IllegalArgumentException("Cell Index Out of Bounds");
-		}else {
-		 return cells.get(cellIndex); 
-		}
-	}
-
-	public void addBrailleCell(int cellOrientation) {
-			cells.add(cellOrientation);		
-	}
-	
-	public void setIncorrectAnswerResponse(String incorrectAnswerResponse) {
-		this.incorrectAnswerResponse = incorrectAnswerResponse;
-	}
-
-	public String getIncorrectAnswerResponse() {return incorrectAnswerResponse;}
-
-	public void setCorrectAnswerResponse(String correctAnswerResponse) {
-		this.correctAnswerResponse = correctAnswerResponse;
-	}
-	
-	public String getCorrectAnswerResponse() {return correctAnswerResponse;}*/
-
 
 }
 
