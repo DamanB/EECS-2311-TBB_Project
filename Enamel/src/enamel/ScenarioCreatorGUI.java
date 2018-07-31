5ee86132063fc4bb86d13957b771a3118b042d23
Damanveer Bharaj <damanbharaj@gmail.com>
2018-07-31 14:04:10 -0400

    Added hotkey functionality

--- Enamel/src/enamel/ScenarioCreatorGUI.java
+++ Enamel/src/enamel/ScenarioCreatorGUI.java
@@ -38,7 +38,7 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
 public class ScenarioCreatorGUI {
-
+	//Contains hotkeys
 	private static JPanel mainPanel;
 	private static JPanel leftBorder;
 	private static JPanel northBorder;
